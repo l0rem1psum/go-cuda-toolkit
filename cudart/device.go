5370e791b30a55cdf4a997a405b772f665668ef5
l0rem1psum <sunshine.xuan5960@gmail.com>
2024-10-08 06:55:01 +0000

    feat(cudart): add some device management API wrappers

--- cudart/device.go
+++ cudart/device.go
@@ -5,6 +5,18 @@ package cudart
 */
 import "C"
 
+func CUDAGetDevice() (int, error) {
+	var device C.int
+	err := cudaErrorToGoError(C.cudaGetDevice(&device))
+	return int(device), err
+}
+
+func CUDAGetDeviceCount() (int, error) {
+	var count C.int
+	err := cudaErrorToGoError(C.cudaGetDeviceCount(&count))
+	return int(count), err
+}
+
 func CUDASetDevice(device int) error {
 	return cudaErrorToGoError(C.cudaSetDevice(C.int(device)))
 }
